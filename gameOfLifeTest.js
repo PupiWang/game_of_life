e92a8d3ee41ce8b7b5178e76f825cced427a41f7
shane51 <suse51@163.com>
2014-10-23 19:12:16 +0800

    Add One Cell Test

--- gameOfLifeTest.js
+++ gameOfLifeTest.js
@@ -8,3 +8,12 @@ describe('Array', function(){
     })
   })
 })
+
+describe('Game', function(){
+  describe('One cell', function(){
+    it('Dead cell should be dead', function(){
+      game = Game.new([0][0])
+      game.next_state.should.equal([0][0])
+    })      
+  })    
+} )
