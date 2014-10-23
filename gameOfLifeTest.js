dc0bd49808f1a42a5b05b313bacb729ed9c75dd5
shane51 <suse51@163.com>
2014-10-23 19:24:52 +0800

    Modify first test case

--- gameOfLifeTest.js
+++ gameOfLifeTest.js
@@ -1,19 +1,10 @@
 var should = require('should');
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    })
-  })
-})
-
 describe('Game', function(){
   describe('One cell', function(){
     it('Dead cell should be dead', function(){
-      game = Game.new([0][0])
-      game.next_state.should.equal([0][0])
+      game = Game.new([[0]])
+      game.next_state.should.equal([0])
     })      
   })    
 } )
