f448594fcc1397318350d5f78aa9a9a4743fc71b
shane51 <suse51@163.com>
2014-10-29 20:59:48 +0800

    All three cell cases

--- ruby/game_of_life_spec.rb
+++ ruby/game_of_life_spec.rb
@@ -1,18 +1,21 @@
 require 'rspec'
 require_relative 'game_of_life'
 
+def assert_grid(init_grid, expected_grid)
+	game = Game.new init_grid
+	expect(game.next_state).to eq(expected_grid) 
+end
+
 describe 'Game' do 
 
   context 'One Cell' do
 
     it 'One dead cell should be dead' do
-      game = Game.new [[0]]
-      expect(game.next_state).to eq([[0]])
+      assert_grid [[0]], [[0]]
     end
 
     it 'One alive cell still be dead' do
-      game = Game.new [[1]]
-      expect(game.next_state).to eq([[0]])
+      assert_grid [[1]], [[0]]
     end
 
  end 
@@ -20,37 +23,39 @@ describe 'Game' do
   context 'Two Cell' do
 
     it 'two dead cells should be dead' do
-      game = Game.new [[0],[0]]
-      expect(game.next_state).to eq([[0],[0]])
+      assert_grid [[0],[0]],
+     			  [[0],[0]]
     end
 
     it 'two alive cells should be dead' do
-      game = Game.new [[1],[1]]
-      expect(game.next_state).to eq([[0],[0]])
+	  assert_grid [[0],[0]],
+     			  [[0],[0]]
     end
 	
 	it 'one alive cell still be dead' do
-      game = Game.new [[0],[1]]
-      expect(game.next_state).to eq([[0],[0]])
-    end
+      assert_grid [[0],[0]],
+     			  [[0],[0]]
+	end
   end
 
 
   context 'Three Cells' do
 
    it 'Three dead cells should be dead' do
-      game = Game.new [[0],[0],[0]]
-      expect(game.next_state).to eq([[0],[0],[0]])
+	   assert_grid [[0],[0],[0]],
+		   		   [[0],[0],[0]]
     end
 
     it 'Three alive cells should be [[1],[1],[1]]' do
-      game = Game.new [[1],[1],[1]]
-      expect(game.next_state).to eq([[0],[1],[0]])
+		assert_grid [[1],[1],[1]],
+				    [[0],[1],[0]]
     end
 
-
-
-
-  
+	it 'Two alive cells should be dead' do
+		assert_grid [[0],[1],[1]],
+				    [[0],[0],[0]]
+		assert_grid [[1],[0],[1]],
+					[[0],[0],[0]]
+    end
   end
 end
