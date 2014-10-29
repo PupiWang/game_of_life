c7bc0caf3eca16402a18ebe1d8ca80e43e05c6b5
shane51 <suse51@163.com>
2014-10-29 20:35:28 +0800

    Add Three cells 2 test cases

--- ruby/game_of_life_spec.rb
+++ ruby/game_of_life_spec.rb
@@ -28,6 +28,29 @@ describe 'Game' do
       game = Game.new [[1],[1]]
       expect(game.next_state).to eq([[0],[0]])
     end
+	
+	it 'one alive cell still be dead' do
+      game = Game.new [[0],[1]]
+      expect(game.next_state).to eq([[0],[0]])
+    end
+  end
 
+
+  context 'Three Cells' do
+
+   it 'Three dead cells should be dead' do
+      game = Game.new [[0],[0],[0]]
+      expect(game.next_state).to eq([[0],[0],[0]])
+    end
+
+    it 'Three alive cells should be [[1],[1],[1]]' do
+      game = Game.new [[0],[1],[0]]
+      expect(game.next_state).to eq([[0],[1],[0]])
+    end
+
+
+
+
+  
   end
 end
