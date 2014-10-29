6a4a801baf36a6de5879dbd81b39ea994d16d301
王智 <dreamjl@live.cn>
2014-10-29 21:16:31 +0800

    refactor

--- ruby/game_of_life.rb
+++ ruby/game_of_life.rb
@@ -1,19 +1,17 @@
 class Game
-  def initialize(init_grid)
-    @init_grid = init_grid
+  def initialize(grid)
+    @grid = grid
+    @grid_length = grid.length
   end
 
-  def next_state
-    if @init_grid.length == 1
-      [[0]]
-    elsif @init_grid.length == 2
-      [[0], [0]]
-    else
-      if @init_grid[0] == [0] || @init_grid[1] == [0]
-        [[0], [0], [0]]
-      else
-        [[0], [1], [0]]
-      end
-    end
+  def tick
+    init_grid
   end
+
+  private
+
+  def init_grid
+    @grid_length.times.map { [0] }
+  end
+
 end
